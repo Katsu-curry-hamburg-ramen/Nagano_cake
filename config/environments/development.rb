120f130856bfd43ce270019a83b6465b6ece6688
EC2 Default User <aria.bravemen@gmail.com>
2022-05-16 07:08:56 +0000

    first commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "645f3109ddfe4af5b9f1452fa3ca7fbc.vfs.cloud9.us-east-1.amazonaws.com"
 end
