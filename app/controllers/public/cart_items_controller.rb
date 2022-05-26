0e82c3ed746a9a105f8bac583178fb9a4e60c094
EC2 Default User <ec2-user@ip-172-31-90-89.ec2.internal>
2022-05-26 06:58:53 +0000

    with

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -17,6 +17,12 @@ class Public::CartItemsController < ApplicationController
         redirect_to public_cart_items_path
     end
     
+    def create
+        @cart_item = CartItem.new(cart_item_params)
+        @cart_item.save
+        redirect_to public_cart_items_path
+    end
+    
     private
   
     def cart_item_params
