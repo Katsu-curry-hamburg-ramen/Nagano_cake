e0ceb65935d06cbbc7e5738de3601aaec128dd36
EC2 Default User <aria.bravemen@gmail.com>
2022-05-29 02:51:56 +0000

    Nagano_cake完成

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,5 +72,5 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  config.hosts << "2d001fe019204baa98629c5f847aa5ef.vfs.cloud9.us-east-1.amazonaws.com"
+  config.hosts << "645f3109ddfe4af5b9f1452fa3ca7fbc.vfs.cloud9.us-east-1.amazonaws.com"
 end
